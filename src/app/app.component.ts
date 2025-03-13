fe7912880e55b4c7991e838854ecdb50ff3e92ec
superdev <superdevp@gmail.com>
2025-03-13 23:04:24 +0200

    a UI for the payments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,9 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'payments';
+  
 }
